4e4d773cd90ea858acd9c212d6644c8ee5cd95a5
Mohamed <mf1456@fayoum.edu.eg>
2022-08-03 09:33:45 +0200

    some changes

--- src/components/UI/ContactItem.jsx
+++ src/components/UI/ContactItem.jsx
@@ -6,9 +6,9 @@ function ContactItem({ name, avatar, bio, id }) {
     console.log(id);
   };
   return (
-    <button onClick={handleClick} className="flex gap-4 items-center p-4 w-full">
+    <button onClick={handleClick} className="flex gap-4 items-center p-4 w-full transition-all hover:bg-light dark:hover:bg-dark">
       <Avatar name={name} img={avatar} />
-      <div>
+      <div className="text-left">
         <h1 className="text-alt-dark dark:text-white font-bold">{name}</h1>
         <p className="text-alt-dark dark:text-white">{bio}</p>
       </div>
